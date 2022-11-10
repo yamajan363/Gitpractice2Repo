46ea62e1f6a874aae8d5ca2aec023b985d04913c
yamajan363 <117695950+yamajan363@users.noreply.github.com>
2022-11-10 17:20:52 -0500

    Update GitReview.java
    main method and review method removed

--- src/gitreview/GitReview.java
+++ src/gitreview/GitReview.java
@@ -2,12 +2,7 @@ package gitreview;
 
 public class GitReview {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-   public void teest() {
-	   System.out.println("this method is on gitreview branch ");
+	
 	   
    }
    
